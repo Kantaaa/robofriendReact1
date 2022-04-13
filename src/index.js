a8bcec8e6cba233f4c8cba2a6d2a99e8c8eefe54
Kanta <Kantasitms1@outlook.com>
2022-04-13 17:27:55 +0200

    added scroll, and refactored the codes :)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
